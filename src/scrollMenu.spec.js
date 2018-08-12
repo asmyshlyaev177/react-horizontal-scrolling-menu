d58128f437540616c09778502c6a3559ee16b47e
Aleksandr Smyshliaev <asmyshlyaev177__a_t__gmail.com>
2018-08-12 10:23:07 +0300

    fix drag issue

--- src/scrollMenu.spec.js
+++ src/scrollMenu.spec.js
@@ -143,7 +143,7 @@ describe('test menu', () => {
   it('set correct innerStyle', () => {
     let style, result;
     result = {
-      width: '9000px',
+      width: '9900px',
       transform: 'translate3d(10px, 0px, 0px)',
       transition: 'transform 0s',
       whiteSpace: 'nowrap',
@@ -786,7 +786,7 @@ describe('functions', () => {
       expect(checkDrag(100, false, 5)).toEqual([0, 0]);
       expect(checkDrag(-200, true, 5)).toEqual([-165, 0]);
       expect(checkDrag(-200, false, 5)).toEqual([-150, 0]);
-      expect(checkDrag(-50, false, 5, null)).toEqual([0, 0]);
+      expect(checkDrag(-50, false, 5, null)).toEqual([-50, 0]);
     });
 
     it('set xPoint from getPoint', () => {
