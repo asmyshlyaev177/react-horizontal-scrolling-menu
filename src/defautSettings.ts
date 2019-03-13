f5df6b03b9c55cec875d00bb93db450999000ae0
Aleksandr Smyshliaev <asmyshlyaev177__a_t__gmail.com>
2019-03-13 14:10:20 +0300

    some comments to code

--- src/defautSettings.ts
+++ src/defautSettings.ts
@@ -1,43 +1,76 @@
 import { CSSProperties } from "react";
 import { MenuProps } from './types';
 
+/**
+ * Default styles for ScrollMenu component
+ */
 const defaultMenuStyle: CSSProperties = {
   display: 'flex',
   alignItems: 'center',
   userSelect: 'none',
 };
 
+/**
+ * Default styles for InnerWrapper component
+ */
 const defaultWrapperStyle: CSSProperties = {
   overflow: 'hidden',
   userSelect: 'none',
 };
 
+/**
+ * Default props for ScrollMenu component
+ */
 const defaultProps: MenuProps = {
+  /** align menu items to center, so in left and right will be empty space */
   alignCenter: true,
+  /** class for Arrow component */
   arrowClass: 'scroll-menu-arrow',
+  /** Arrows components */
   arrowLeft: null,
   arrowRight: null,
+  /** class for arrow when it's disabled */
   arrowDisabledClass: 'scroll-menu-arrow--disabled',
+  /** when drag item and mouse button mouseup choose menu item under cursor  */
   clickWhenDrag: false,
+  /** enable/disable dragging with mouse */
   dragging: true,
+  /** array of MenuItem elements */
   data: [],
   forwardClick: false,
+  /** class for InnerWrapper */
   innerWrapperClass: 'menu-wrapper--inner',
+  /** class for MenuItem */
   itemClass: 'menu-item-wrapper',
+  /** class for selected MenuItem */
   itemClassActive: 'active',
+  /** add disabled class to arrows */
   hideArrows: false,
+  /** hide left/right arrow on left/right edge */
   hideSingleArrow: false,
+  /** class for ScrollMenu */
   menuClass: 'horizontal-menu',
+  /** styles for ScrollMenu */
   menuStyle: defaultMenuStyle,
+  /** cb when item selected */
   onSelect: () => false,
+  /** cb when position updated */
   onUpdate: () => false,
+  /** automatically scroll to selected item on initialization */
   scrollToSelected: false,
+  /** selected menu item */
   selected: '',
+  /** position of elements */
   translate: 0.0,
+  /** animation speed */
   transition: 0.4,
+  /** class for wrapper */
   wrapperClass: 'menu-wrapper',
+  /** styles for wrapper */
   wrapperStyle: defaultWrapperStyle,
+  /** scroll with mouse wheel */
   wheel: true,
+  /** this not used */
   xPoint: 0,
 };
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 import { Ref } from "./types";
 
+/** check if value not empty */
 const notUndefOrNull = (val: any) => val !== undefined && val !== null;
+/** getClientRect */
 const getClientRect = (elem: Ref): {width: number, x: number} => {
   if (!elem || !elem.getBoundingClientRect || typeof elem.getBoundingClientRect !== 'function') return {width: 0, x: 0};
 
@@ -8,14 +10,18 @@ const getClientRect = (elem: Ref): {width: number, x: number} => {
   return {width, x: +left};
 };
 
+/** format translate value */
 const formatTranslate = (val: number): number => +val.toFixed(3);
+/** check if translate is valid */
 const translateIsValid = (val: any): boolean =>
   typeof val === 'number' && !isNaN(+val);
+/** pass translate value and default, valid - return formatted value, not valid - return default */
 const validateTranslate = (value: any, defaultValue: number): number =>
   translateIsValid(value)
     ? formatTranslate(value)
     : formatTranslate(defaultValue);
 
+/** test passive event support, for performance */
 const testPassiveEventSupport = (): boolean => {
   let passiveSupported = false;
 
