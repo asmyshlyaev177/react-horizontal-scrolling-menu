3eaffe7c40fe1b69cedd40eb449821422188b759
Aleksandr Smyshlyaev <aleksandr@omise.co>
2020-01-11 19:00:08 +0700

    Updates

---
+++ precache-manifest.1b2917ff8774418917070c1d77551556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d41401aab16171e68bbff88fd90d92",
+    "url": "/react-horizontal-scrolling-menu/index.html"
+  },
+  {
+    "revision": "28c682568b467b9c2fbe",
+    "url": "/react-horizontal-scrolling-menu/static/css/main.6411d436.chunk.css"
+  },
+  {
+    "revision": "f154b5a2487f03febf91",
+    "url": "/react-horizontal-scrolling-menu/static/js/2.cdd1e05d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-horizontal-scrolling-menu/static/js/2.cdd1e05d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28c682568b467b9c2fbe",
+    "url": "/react-horizontal-scrolling-menu/static/js/main.8feab8a4.chunk.js"
+  },
+  {
+    "revision": "cc15cebc226bba2e44f7",
+    "url": "/react-horizontal-scrolling-menu/static/js/runtime-main.0fc42ed1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-horizontal-scrolling-menu/static/media/logo.5d5d9eef.svg"
+  }
+]);
