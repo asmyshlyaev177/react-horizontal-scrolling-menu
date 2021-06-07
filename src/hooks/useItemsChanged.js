be4a941feab7e65c7af9cbaa89af45d23ec9c3c0
Aleksandr Smyshlyaev <asmyshlyaev177@gmail.com>
2021-06-07 16:57:24 +0700

    useItemsChanged test

--- src/hooks/useItemsChanged.js
+++ src/hooks/useItemsChanged.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 import { id as itemId } from '../constants'
 
-// TODO: tests
-function useItemsChanged(menuItems, refs) {
+function useItemsChanged(menuItems, refs = {}) {
   const [hash, setHash] = React.useState('')
 
   React.useLayoutEffect(() => {
-    const newHash = React.Children.toArray(menuItems)
+    const newHash = (menuItems ? React.Children.toArray(menuItems) : [])
       .map((c) => c?.props?.[itemId])
       .filter(Boolean)
       .join('')
