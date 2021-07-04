9aa77558e0f9aa6f58a949c8cc8396897eb5f707
Aleksandr Smyshlyaev <asmyshlyaev177@gmail.com>
2021-07-04 18:53:24 +0200

    updated example-nextjs and use it as main example

--- example-nextjs/pages/_app.js
+++ example-nextjs/pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+// NOTE: hide scrollbar on menu component
+// import '../styles/hideScrollbar.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
