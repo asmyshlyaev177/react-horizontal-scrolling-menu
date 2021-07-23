21938d612d7bebdb06ac051a7cb3ae45ed91e354
Aleksandr Smyshlyaev <asmyshlyaev177@gmail.com>
2021-07-23 15:27:57 +0200

    Updates

--- _next/static/zJHMIPLjDiAivtWZ6Kyzh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-09556879df47e7687869.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-60933b1bffcf3051bb89.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
