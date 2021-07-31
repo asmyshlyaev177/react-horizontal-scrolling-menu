2910997866f37d5faf249081c219c6ab8c2ba36e
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2021-07-31 10:15:20 +0200

    notes about hide scrollbar and styles for alter arrows position

--- example-nextjs/pages/_app.js
+++ example-nextjs/pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/globals.css';
 // NOTE: hide scrollbar on menu component
 // import '../styles/hideScrollbar.css';
 
+// NOTE: change arrows position
+// import '../styles/arrowsOnBottom.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
