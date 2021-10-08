c72d51771bf13a4ea0f1238487ed60f9cd79afce
Aleksandr Smyshlyaev <asmyshlyaev177@gmail.com>
2021-10-08 13:17:54 +0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0379e55ea62a61fa261e";
+/******/ 	var hotCurrentHash = "093e9b1225025a610c56";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
