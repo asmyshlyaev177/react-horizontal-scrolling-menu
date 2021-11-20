bb15e1cd7246b4cc947a7f79292578ff0e39a19e
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2021-11-20 17:25:10 +0400

    refactor: renamed useHideBodyScroll to usePreventBodyScroll, now works without styles

---
+++ example-nextjs/helpers/usePreventBodyScroll/index.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const preventDefault = (ev: Event) => {
+  if (ev.preventDefault) {
+    ev.preventDefault();
+  }
+  ev.returnValue = false;
+};
+
+const enableBodyScroll = () => {
+  document && document.removeEventListener('wheel', preventDefault, false);
+};
+const disableBodyScroll = () => {
+  document &&
+    document.addEventListener('wheel', preventDefault, {
+      passive: false,
+    });
+};
+
+function usePreventBodyScroll() {
+  const [hidden, setHidden] = React.useState(false);
+
+  React.useEffect(() => {
+    hidden ? disableBodyScroll() : enableBodyScroll();
+
+    return enableBodyScroll;
+  }, [hidden]);
+
+  const disableScroll = React.useCallback(() => setHidden(true), []);
+  const enableScroll = React.useCallback(() => setHidden(false), []);
+  return { disableScroll, enableScroll };
+}
+
+export default usePreventBodyScroll;
