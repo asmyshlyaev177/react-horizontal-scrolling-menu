8f88a5a0f4561b3db64bc543051db1d87d4734fd
Aleksandr Smyshlyaev <asmyshlyaev177@gmail.com>
2021-11-20 17:05:36 +0400

    chore: removed safari-polyfill from nextjs example

--- example-nextjs/helpers/safari-polyfill.ts
+++
@@ -1,5 +0,0 @@
-import { elementScrollIntoViewPolyfill } from 'seamless-scroll-polyfill';
-
-if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-  elementScrollIntoViewPolyfill();
-}
--- example-nextjs/pages/index.tsx
+++ example-nextjs/pages/index.tsx
@@ -1,5 +1,3 @@
-import '../helpers/safari-polyfill';
-
 import React from 'react';
 
 import throttle from 'lodash/throttle';
