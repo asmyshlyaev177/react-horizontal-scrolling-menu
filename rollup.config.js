1dc5ebdebe6976f749c52c5f5ee394662b6ee016
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2023-04-14 17:07:17 +0400

    fix: change mjs to be browser and main entries

--- rollup.config.js
+++ rollup.config.js
@@ -43,7 +43,7 @@ export default [
     input,
     output: {
       name: 'react-horizontal-scrolling-menu',
-      file: pkg.browser,
+      file: pkg.unpkg,
       format: 'umd',
       sourcemap,
       globals: {
