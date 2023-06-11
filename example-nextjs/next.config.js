bb7765b00d8dbff41a9419179f681687b773bd5a
Aleksandr Smyshliaev <asmyshlyaev177@gmail.com>
2023-06-11 11:05:27 +0400

    chore: comment about setup inmprovement

--- example-nextjs/next.config.js
+++ example-nextjs/next.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require('next-transpile-modules')(
   ['react-horizontal-scrolling-menu'],
   { resolveSymlinks: false }
@@ -6,9 +7,26 @@ const withTM = require('next-transpile-modules')(
 const isProd = process.env.NODE_ENV === 'production';
 console.log({ isProd });
 
-module.exports = isProd
-  ? {
-    assetPrefix: '/react-horizontal-scrolling-menu/',
-    basePath: '/react-horizontal-scrolling-menu',
-  }
-  : withTM();
+module.exports = {
+  ...(isProd
+    ? {
+        assetPrefix: '/react-horizontal-scrolling-menu/',
+        basePath: '/react-horizontal-scrolling-menu',
+      }
+    : withTM()),
+
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     include: /node_modules/,
+  //     test: /\.mjs$/,
+  //     type: 'javascript/auto',
+  //   });
+
+  //   return config;
+  // },
+};
+
+// TODO: try yarn workspaces and vite ?
+// https://dev.to/nicolaserny/create-a-react-component-library-with-vite-and-typescript-1ih9
+// or https://github.com/nicolaserny/react-library-vite-example
+// and https://articles.wesionary.team/react-component-library-with-vite-and-deploy-in-npm-579c2880d6ff
