4626563ae45ee748d4afb7f1da0b0abe83fbca8a
Aleksandr Smyshlyeav <asmyshlyaev177@gmail.com>
2023-12-11 13:30:14 +0400

    ci(storybook): run storybook tests only inside stories

---
+++ test-runner-jest.config.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getJestConfig } = require('@storybook/test-runner');
+
+// The default Jest configuration comes from @storybook/test-runner
+const testRunnerConfig = getJestConfig();
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+module.exports = {
+  ...testRunnerConfig,
+  roots: ['stories'],
+  /** Add your own overrides below, and make sure
+   *  to merge testRunnerConfig properties with your own
+   * @see https://jestjs.io/docs/configuration
+   */
+};
