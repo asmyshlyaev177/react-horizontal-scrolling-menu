947b145e7135349f0d23142e3a9abceeeb2b5bfc
Aleksandr Smyshlyeav <asmyshlyaev177@gmail.com>
2023-12-11 10:59:45 +0400

    chore(storybook): updated scripts

--- stories/Simple/Simple.stories.tsx
+++ stories/Simple/Simple.stories.tsx
@@ -1,19 +1,16 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react';
 import type { Meta } from '@storybook/react';
 import { createLiveEditStory } from 'storybook-addon-code-editor';
 
 import { ScrollMenu } from '../../src/index';
 import * as Lib from '../../src/index';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+
 // @ts-ignore
-import * as Types from '../../dist/index.d.ts?raw';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+import * as Types from '../index.d.ts?raw';
 // @ts-ignore
 import * as styles from './index.css';
 
-// TODO: move from up from src folder
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Example from './Simple.source.tsx?raw';
 
@@ -38,14 +35,22 @@ const meta: Meta<typeof ScrollMenu> = {
 
 export default meta;
 
+export const Simple = createLiveEditStory({
+  code: Example,
+  availableImports: {
+    react: React,
+    'react-horizontal-scrolling-menu': Lib,
+    './index.css': styles,
+  },
+  modifyEditor: setupEditor,
+});
+
 type args = Parameters<
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   Parameters<typeof createLiveEditStory>[0]['modifyEditor']
 >;
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const setupEditor = (monaco: args[0], _editor: args[1]) => {
+function setupEditor(monaco: args[0], _editor: args[1]) {
   monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
     noSemanticValidation: false,
     noSyntaxValidation: false,
@@ -63,14 +68,4 @@ const setupEditor = (monaco: args[0], _editor: args[1]) => {
   monaco.languages.typescript.typescriptDefaults.addExtraLib(
     `declare module "react-horizontal-scrolling-menu" { ${Types.default} }`
   );
-};
-
-export const Simple = createLiveEditStory({
-  code: Example,
-  availableImports: {
-    react: React,
-    'react-horizontal-scrolling-menu': Lib,
-    './index.css': styles,
-  },
-  modifyEditor: setupEditor,
-});
+}
