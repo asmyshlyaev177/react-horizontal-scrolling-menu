dcb1aaed74cbb83472c809549791fb226dbe7148
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-02-08 14:54:23 +0400

    chore(cleanup): remove webpack leftovers

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    'transform-react-remove-prop-types',
-    'dynamic-import-node',
-  ],
-}
