546f39de96bbc5d382875238c36c4ba53acd9b0c
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-10-07 09:57:12 +0400

    revert: revert changes related to #289
    Need to investigate it more deeply, Chrome and FF have very different behavior

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -3,36 +3,31 @@ import React from 'react';
 import { ItemsMap } from '../ItemsMap';
 import { getNodesFromRefs, observerEntriesToItems } from '../helpers';
 import { observerOptions } from '../settings';
-import { type Refs, type Item } from '../types';
 
 import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
+
+import type { Refs } from '../types';
+
+interface Props {
+  items: ItemsMap;
+  itemsChanged: string;
+  options: typeof observerOptions;
+  refs: Refs;
+}
 
 function useIntersectionObserver({
   items,
   itemsChanged,
   refs,
   options,
-  wrapperVisible,
 }: Props) {
-  const observer = React.useRef<IntersectionObserver>();
-
-  const { schedule, add, addPriority, cleanup } = useQueue(
-    items,
-    wrapperVisible,
-  );
+  const observer: { current?: IntersectionObserver } = React.useRef();
 
   const ioCb = React.useCallback(
     (entries: IntersectionObserverEntry[]) => {
-      const parsedEntries = observerEntriesToItems(entries, options);
-
-      if (wrapperVisible.current) {
-        addPriority(parsedEntries);
-      } else {
-        add(parsedEntries);
-      }
-      schedule();
+      items.setBatch(observerEntriesToItems(entries, options));
     },
-    [options, wrapperVisible, addPriority, schedule, add],
+    [items, options],
   );
 
   useIsomorphicLayoutEffect(() => {
@@ -45,111 +40,8 @@ function useIntersectionObserver({
     return () => {
       observerInstance.disconnect();
       observer.current = undefined;
-      cleanup();
     };
-  }, [ioCb, itemsChanged, options, refs, cleanup]);
+  }, [ioCb, itemsChanged, options, refs]);
 }
-
-const useQueue = (items: ItemsMap, wrapperVisible: { current: boolean }) => {
-  const queue = React.useRef<Item[]>([]);
-  const priorityQueue = React.useRef<Item[]>([]);
-  const timer = React.useRef(0 as unknown as NodeJS.Timeout);
-  const running = React.useRef(false);
-
-  const isVisible = React.useCallback(() => {
-    return wrapperVisible.current;
-  }, [wrapperVisible]);
-
-  const cancel = React.useCallback(() => {
-    clearTimeout(timer.current);
-    running.current = false;
-  }, [timer, running]);
-
-  const run = React.useCallback(() => {
-    if (!isVisible()) {
-      cancel();
-      schedule();
-      return;
-    }
-
-    items.setBatch(
-      filterQueue(queue.current, dedupeEntries(priorityQueue.current)),
-    );
-  }, [cancel, queue, priorityQueue, isVisible, items]);
-
-  const schedule = React.useCallback(() => {
-    if (running.current) {
-      return;
-    }
-    running.current = true;
-    timer.current = setTimeout(
-      () => {
-        run();
-        queue.current = [];
-        priorityQueue.current = [];
-        running.current = false;
-      },
-      isVisible() ? 0 : 100,
-    );
-  }, [run, priorityQueue, queue, isVisible]);
-
-  const cleanup = React.useCallback(() => {
-    cancel();
-    queue.current = [];
-    priorityQueue.current = [];
-  }, [priorityQueue, queue, cancel]);
-
-  const add = React.useCallback(
-    (entries: Item[]) => {
-      cancel();
-      queue.current.push(...entries);
-      schedule();
-    },
-    [queue, schedule, cancel],
-  );
-
-  const addPriority = React.useCallback(
-    (entries: Item[]) => {
-      cancel();
-      priorityQueue.current.push(...entries);
-      schedule();
-    },
-    [priorityQueue, schedule, cancel],
-  );
-
-  return {
-    schedule,
-    add,
-    addPriority,
-    running,
-    cleanup,
-  };
-};
-
-const filterQueue = (queue: Item[], priorityEntries: Item[]) => {
-  return dedupeEntries(queue)
-    .filter((it) => !priorityEntries.find((en) => en[0] === it[0]))
-    .concat(priorityEntries);
-};
-
-const dedupeEntries = (entries: Item[]) => {
-  const result: Item[] = [];
-
-  entries.reverse().forEach((en) => {
-    if (!result.find((it) => it[0] === en[0])) {
-      result.push(en);
-    }
-  });
-
-  return result.reverse();
-};
 
 export default useIntersectionObserver;
-
-interface Props {
-  items: ItemsMap;
-  itemsChanged: string;
-  options: typeof observerOptions;
-  refs: Refs;
-  wrapperVisible: { current: boolean };
-}
