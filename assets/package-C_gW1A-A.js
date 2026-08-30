6ab40e6627176bcd09796d5d54e3560303ccc2fb
asmyshlyaev177 <asmyshlyaev177@users.noreply.github.com>
2026-08-30 10:05:55 +0000

    deploy: aa675b5aeef1c035339c09220a2902bd947fad25

--- assets/package-C_gW1A-A.js
+++ assets/package-C_gW1A-A.js
@@ -1,6 +1,6 @@
 import{n as e}from"./rolldown-runtime-DkW27tQK.js";var t;function n(){return(n=e((()=>{t=`{
   "name": "react-horizontal-scrolling-menu",
-  "version": "8.3.0",
+  "version": "8.3.1",
   "author": {
     "name": "Aleksandr Smyshliaev",
     "email": "asmyshlyaev177@gmail.com",
