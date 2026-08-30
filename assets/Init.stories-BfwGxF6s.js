8ef859064663fb55e3eb95539ae341a09950e068
asmyshlyaev177 <asmyshlyaev177@users.noreply.github.com>
2026-08-30 12:14:29 +0000

    deploy: c8dcdfc65f30d4fa6597d66ba2750f76005fc54a

--- assets/Init.stories-BfwGxF6s.js
+++ assets/Init.stories-BfwGxF6s.js
@@ -1,4 +1,4 @@
-import{a as e,n as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./react-BZJXY1be.js";import{t as r}from"./jsx-runtime-DeHZSEgm.js";import{_ as i,a,g as o,i as s,n as c,o as l,r as u,s as d,t as f,v as p,y as m}from"./dist-C8yMuzvZ.js";import{n as h,t as g}from"./SizeWrapper-BzDG7ebJ.js";import{a as _,t as v}from"./test-CHHlmwCw.js";function y(){let[e]=T.useState(()=>j()),[t,n]=T.useState([]),r=e=>!!t.find(t=>t===e),i=e=>{let t=r(e);n(n=>t?n.filter(t=>t!==e):n.concat(e))};return(0,E.jsxs)(`div`,{children:[(0,E.jsx)(`div`,{children:(0,E.jsx)(D,{children:(0,E.jsx)(u,{LeftArrow:b,RightArrow:x,onWheel:w,children:e.map(({id:e})=>(0,E.jsx)(C,{title:e,itemId:e,onClick:()=>i(e),selected:r(e)},e))})})}),(0,E.jsx)(`div`,{style:{height:`300vh`,backgroundColor:`aqua`,opacity:.2},children:`filler`})]})}function b(){let e=T.useContext(f),t=e.useLeftArrowVisible();return(0,E.jsx)(S,{disabled:t,onClick:()=>e.scrollPrev(),testId:`left-arrow`,children:`Left`})}function x(){let e=T.useContext(f),t=e.useRightArrowVisible();return(0,E.jsx)(S,{disabled:t,onClick:()=>e.scrollNext(),testId:`right-arrow`,children:`Right`})}function S({children:e,disabled:t,onClick:n,className:r,testId:i}){return(0,E.jsx)(O,{disabled:t,onClick:n,className:`arrow-${r}`,"data-testid":i,children:e})}function C({onClick:e,selected:t,title:n,itemId:r}){let i=T.useContext(f),a=i.useIsVisible(r,!0);return(0,E.jsxs)(k,{"data-cy":r,onClick:()=>e(i),onKeyDown:t=>{t.code===`Enter`&&e(i)},"data-testid":`card`,role:`button`,tabIndex:0,className:`card`,visible:a,selected:t,children:[(0,E.jsxs)(`div`,{className:`header`,children:[(0,E.jsx)(`div`,{children:n}),(0,E.jsxs)(`div`,{className:`visible`,children:[`visible: `,JSON.stringify(a)]}),(0,E.jsxs)(`div`,{className:`selected`,children:[`selected: `,JSON.stringify(!!t)]})]}),(0,E.jsx)(`div`,{className:`background`})]})}function w(e,t){if(Math.abs(t.deltaX)!==0||Math.abs(t.deltaY)<15){t.stopPropagation();return}t.deltaY<0?e.scrollNext():e.scrollPrev()}var T,E,D,O,k,A,j;function M(){return(M=t((()=>{o(),T=e(n(),1),c(),E=r(),D=i(`div`)({"& .react-horizontal-scrolling-menu--scroll-container::-webkit-scrollbar":{display:`none`},"& .react-horizontal-scrolling-menu--scroll-container":{scrollbarWidth:`none`,"-ms-overflow-style":`none`}}),O=i(`button`)(e=>({cursor:`pointer`,display:`flex`,flexDirection:`column`,justifyContent:`center`,marginBottom:`2px`,opacity:e.disabled?`0`:`1`,userSelect:`none`,borderRadius:`6px`,borderWidth:`1px`})),k=i(`div`)(e=>({border:`1px solid`,display:`inline-block`,margin:`0 10px`,width:`160px`,userSelect:`none`,borderRadius:`8px`,overflow:`hidden`,"& .header":{backgroundColor:`white`},"& .visible":{backgroundColor:e.visible?`transparent`:`gray`},"& .background":{backgroundColor:e.selected?`green`:`bisque`,height:`200px`}})),A=e=>`test${e}`,j=()=>Array(10).fill(0).map((e,t)=>({id:A(t)})),y.__docgenInfo={description:``,methods:[],displayName:`SimpleExample`}})))()}var N;function P(){return(P=t((()=>{N=`import 'react-horizontal-scrolling-menu/dist/styles.css';
+import{a as e,n as t}from"./rolldown-runtime-DkW27tQK.js";import{t as n}from"./react-BZJXY1be.js";import{t as r}from"./jsx-runtime-DeHZSEgm.js";import{_ as i,a,g as o,i as s,n as c,o as l,r as u,s as d,t as f,v as p,y as m}from"./dist-xJ0qTjfS.js";import{n as h,t as g}from"./SizeWrapper-DFvFoyVR.js";import{a as _,t as v}from"./test-CHHlmwCw.js";function y(){let[e]=T.useState(()=>j()),[t,n]=T.useState([]),r=e=>!!t.find(t=>t===e),i=e=>{let t=r(e);n(n=>t?n.filter(t=>t!==e):n.concat(e))},[a,o]=T.useState(!1);return(0,E.jsxs)(`div`,{style:{opacity:+a},children:[(0,E.jsx)(`div`,{children:(0,E.jsx)(D,{children:(0,E.jsx)(u,{LeftArrow:b,RightArrow:x,onInit:()=>o(!0),onWheel:w,children:e.map(({id:e})=>(0,E.jsx)(C,{title:e,itemId:e,onClick:()=>i(e),selected:r(e)},e))})})}),(0,E.jsx)(`div`,{style:{height:`300vh`,backgroundColor:`aqua`,opacity:.2},children:`filler`})]})}function b(){let e=T.useContext(f),t=e.useLeftArrowVisible();return(0,E.jsx)(S,{disabled:t,onClick:()=>e.scrollPrev(),testId:`left-arrow`,children:`Left`})}function x(){let e=T.useContext(f),t=e.useRightArrowVisible();return(0,E.jsx)(S,{disabled:t,onClick:()=>e.scrollNext(),testId:`right-arrow`,children:`Right`})}function S({children:e,disabled:t,onClick:n,className:r,testId:i}){return(0,E.jsx)(O,{disabled:t,onClick:n,className:`arrow-${r}`,"data-testid":i,children:e})}function C({onClick:e,selected:t,title:n,itemId:r}){let i=T.useContext(f),a=i.useIsVisible(r,!0);return(0,E.jsxs)(k,{"data-cy":r,onClick:()=>e(i),onKeyDown:t=>{t.code===`Enter`&&e(i)},"data-testid":`card`,role:`button`,tabIndex:0,className:`card`,visible:a,selected:t,children:[(0,E.jsxs)(`div`,{className:`header`,children:[(0,E.jsx)(`div`,{children:n}),(0,E.jsxs)(`div`,{className:`visible`,children:[`visible: `,JSON.stringify(a)]}),(0,E.jsxs)(`div`,{className:`selected`,children:[`selected: `,JSON.stringify(!!t)]})]}),(0,E.jsx)(`div`,{className:`background`})]})}function w(e,t){if(Math.abs(t.deltaX)!==0||Math.abs(t.deltaY)<15){t.stopPropagation();return}t.deltaY<0?e.scrollNext():e.scrollPrev()}var T,E,D,O,k,A,j;function M(){return(M=t((()=>{o(),T=e(n(),1),c(),E=r(),D=i(`div`)({"& .react-horizontal-scrolling-menu--scroll-container::-webkit-scrollbar":{display:`none`},"& .react-horizontal-scrolling-menu--scroll-container":{scrollbarWidth:`none`,"-ms-overflow-style":`none`}}),O=i(`button`)(e=>({cursor:`pointer`,display:`flex`,flexDirection:`column`,justifyContent:`center`,marginBottom:`2px`,opacity:e.disabled?`0`:`1`,userSelect:`none`,borderRadius:`6px`,borderWidth:`1px`})),k=i(`div`)(e=>({border:`1px solid`,display:`inline-block`,margin:`0 10px`,width:`160px`,userSelect:`none`,borderRadius:`8px`,overflow:`hidden`,"& .header":{backgroundColor:`white`},"& .visible":{backgroundColor:e.visible?`transparent`:`gray`},"& .background":{backgroundColor:e.selected?`green`:`bisque`,height:`200px`}})),A=e=>`test${e}`,j=()=>Array(10).fill(0).map((e,t)=>({id:A(t)})),y.__docgenInfo={description:``,methods:[],displayName:`NoBlink`}})))()}var N;function P(){return(P=t((()=>{N=`import 'react-horizontal-scrolling-menu/dist/styles.css';
 
 import styled from '@emotion/styled';
 import React from 'react';
@@ -8,7 +8,7 @@ import {
   VisibilityContext,
 } from 'react-horizontal-scrolling-menu';
 
-export function SimpleExample() {
+export function NoBlink() {
   const [items] = React.useState(() => getItems());
   const [selected, setSelected] = React.useState<string[]>([]);
 
@@ -25,13 +25,17 @@ export function SimpleExample() {
     );
   };
 
+  // NOTE: get rid of blinking on init
+  const [init, setInit] = React.useState(false);
+
   return (
-    <div>
+    <div style={{ opacity: +init }}>
       <div>
         <NoScrollbar>
           <ScrollMenu
             LeftArrow={LeftArrow}
             RightArrow={RightArrow}
+            onInit={() => setInit(true)}
             onWheel={onWheel}
           >
             {items.map(({ id }) => (
@@ -54,7 +58,7 @@ export function SimpleExample() {
   );
 }
 
-export default SimpleExample;
+export default NoBlink;
 
 const NoScrollbar = styled('div')({
   '& .react-horizontal-scrolling-menu--scroll-container::-webkit-scrollbar': {
@@ -220,7 +224,7 @@ function onWheel(apiObj: publicApiType, ev: React.WheelEvent): void {
     apiObj.scrollPrev();
   }
 }
-`})))()}var F,I,L,R,z;function B(){return(B=t((()=>{n(),p(),d(),s(),h(),_(),M(),P(),F=r(),I={title:`Examples/Simple`,component:y,decorators:[e=>(0,F.jsx)(g,{children:(0,F.jsx)(e,{})})]},L={},m(L,{code:N,availableImports:l,modifyEditor:a}),R={...v(),tags:[`test-only`]},L.parameters={...L.parameters,docs:{...L.parameters?.docs,source:{originalSource:`{}`,...L.parameters?.docs?.source}}},R.parameters={...R.parameters,docs:{...R.parameters?.docs,source:{originalSource:`{
+`})))()}var F,I,L,R,z;function B(){return(B=t((()=>{n(),p(),d(),s(),h(),_(),M(),P(),F=r(),I={title:`Examples/InitNoBlink`,component:y,decorators:[e=>(0,F.jsx)(g,{children:(0,F.jsx)(e,{})})]},L={},m(L,{code:N,availableImports:l,modifyEditor:a}),R={...v(),tags:[`test-only`]},L.parameters={...L.parameters,docs:{...L.parameters?.docs,source:{originalSource:`{}`,...L.parameters?.docs?.source}}},R.parameters={...R.parameters,docs:{...R.parameters?.docs,source:{originalSource:`{
   ...ScrollTest(),
   tags: ['test-only']
-}`,...R.parameters?.docs?.source}}},z=[`Simple`,`Test`]})))()}B();export{L as Simple,R as Test,z as __namedExportsOrder,I as default};
+}`,...R.parameters?.docs?.source}}},z=[`Init`,`Test`]})))()}B();export{L as Init,R as Test,z as __namedExportsOrder,I as default};
--- assets/package-DofwUygD.js
+++ assets/package-DofwUygD.js
@@ -1,6 +1,6 @@
 import{n as e}from"./rolldown-runtime-DkW27tQK.js";var t;function n(){return(n=e((()=>{t=`{
   "name": "react-horizontal-scrolling-menu",
-  "version": "8.3.1",
+  "version": "8.3.2",
   "author": {
     "name": "Aleksandr Smyshliaev",
     "email": "asmyshlyaev177@gmail.com",
